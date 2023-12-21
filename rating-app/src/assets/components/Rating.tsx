70cd81cbdb2a211d044da3413f558ef9ec002a60
Rebecca Boateng <r.y.boateng@outlook.com>
2023-12-21 15:58:29 +0000

    rating screen complete

--- rating-app/src/assets/components/Rating.tsx
+++ rating-app/src/assets/components/Rating.tsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import styles from "./Rating.module.css";
 
 export function Rating() {
+const [selectedRating, setSelectedRating] = useState<number>();
+
+function handleRatingClicked(rating:number) {
+setSelectedRating(rating) 
+}
   return (
     <div className={styles.panel}>
-        <img src="/icon-star.svg"></img> 
-      <h1>How did we do?</h1>
-      <p>
+      <img className={styles.star} src="/icon-star.svg"></img>
+      <h1 className={styles.title}>How did we do?</h1>
+      <p className={styles.description}>
         Please let us know how we did with your support request. All feedbck is
-        appreciated to help us improve our service.
+        appreciated to help us improve our service!
       </p>
 
-      <button> Submit</button>
+      <div className={styles.group}> 
+      {[1, 2, 3, 4, 5].map((rating) => ( 
+         <button 
+         onClick={()=> handleRatingClicked(rating)}
+          className={styles.rating}
+          > 
+          {rating} 
+          </button>
+      ))}
+     </div>
+
+      <button  disabled={selectedRating ===undefined} className={styles.submit}> Submit</button>
     </div>
   );
 }
